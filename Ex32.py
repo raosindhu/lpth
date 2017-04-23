0c9ad3b59d86a6570c93fd4dc526e877af8fb5e0
sindhu <sindhureddy.195@gmail.com>
2017-04-22 18:06:45 -0700

    Ex

---
+++ Ex32.py
@@ -0,0 +1,14 @@
+the_count = [1,2,3,4,5]
+fruit = ['apple', 'orange', 'banana', 'pears']
+for number in the_count:
+    print "This is the count %d" %number
+
+for fruit_name in fruit:
+    print "The fruit is %s" %fruit_name
+
+new_file = []
+
+for i in range(0,6):
+    print "Adding %d to the %s" %(i, new_file)
+    new_file.append(i)
+
