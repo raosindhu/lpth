3c4b5da00798d4fd7490e965456510d602e49de8
sindhu <sindhureddy.195@gmail.com>
2017-04-22 18:06:26 -0700

    Ex33

---
+++ Ex33.py
@@ -0,0 +1,18 @@
+i = 0
+
+new_list = []
+
+while i < 6:
+    print "Now the number is %d" %i
+    new_list.append(i)
+
+    i = i + 1
+    print "The number is changed to: %d" %i
+
+for i in new_list:
+    print "The number is %d" %i
+
+
+
+
+
